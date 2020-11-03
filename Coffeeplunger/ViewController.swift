16a0582064f0f11ff78acbb30c7ba64db08d9c1b
Eddy Rogier <eddyrogier@gmail.com>
2020-11-03 18:53:28 +0100

    create UI

--- Coffeeplunger/ViewController.swift
+++ Coffeeplunger/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var water: UILabel!
+    @IBOutlet weak var coffee: UILabel!
+    @IBOutlet weak var people: UILabel!
+    
+    // private
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func power(_ sender: UIButton) {
+        print("██░░░ L\(#line) 🚧📕 tap 🚧🚧 [ \(type(of: self))  \(#function) ]")
+    }
+    
+    @IBAction func sliderPerson(_ sender: UISlider) {
+    
+        people.text = String(Int(roundf(sender.value)))
+    }
+    
 }
 
